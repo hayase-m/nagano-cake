430889d66c4102a4d64f23c496fbf198583294f2
hayasemaru <mrym.124.hys@gmail.com>
2025-02-28 12:15:04 +0900

    顧客のアクティブ判定をチェック

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
+
+  private
+  # アクティブであるかを判断するメソッド
+  def customer_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    customer = Customer.find_by(email: params[:customer][:email])
+    # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
+    return if customer.nil?
+    # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless customer.valid_password?(params[:customer][:password])
+    # 【処理内容4】 アクティブでない会員に対する処理
+    return if is_active? #is_activeがtrueならメソッドを終了
+    redirect_to new_customer_registration_path #falseならsign_upに移動
+  end
   # GET /resource/sign_in
   # def new
   #   super
