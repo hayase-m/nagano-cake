b546ba2438296d924f698c67ef9946b096d3369f
hayasemaru <mrym.124.hys@gmail.com>
2025-03-05 00:18:49 +0900

    トップページ作成（ジャンル検索未実装）、kaminariのインストール

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ApplicationRecord
-  has_one_attached : item_image
 
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+
+  def tax_included_price
+    "¥#{(price * 1.1).round(0)}"
+  end
+
+  has_one_attached :item_image
+
+  def get_item_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit:[width, height]).processed
+  end
 end
