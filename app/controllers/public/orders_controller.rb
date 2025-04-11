0efb05a666f9dfbf9819db477fd86cab7aae9ac7
hayasemaru <mrym.124.hys@gmail.com>
2025-04-11 14:36:32 +0900

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,21 +1,17 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def new
+    @customer = current_customer
   end
 
-  def confirm
-  end
+  def confirm; end
 
-  def thanks
-  end
+  def thanks; end
 
-  def create
-  end
+  def create; end
 
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,8 +3,8 @@
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
 
-
   private
+
   # アクティブであるかを判断するメソッド
   def customer_state
     # 【処理内容1】 入力されたemailからアカウントを1件取得
@@ -14,19 +14,20 @@ class Public::SessionsController < Devise::SessionsController
     # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
     return unless customer.valid_password?(params[:customer][:password])
     # 【処理内容4】 アクティブでない会員に対する処理
-    return if is_active? #is_activeがtrueならメソッドを終了
-    redirect_to new_customer_registration_path #falseならsign_upに移動
+    return if is_active? # is_activeがtrueならメソッドを終了
+
+    redirect_to new_customer_registration_path # falseならsign_upに移動
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     root_path
   end
 
-    # ログアウト後のリダイレクト先
-  def after_sign_out_path_for(resource)
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(_resource)
     root_path
   end
-  
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -45,7 +46,7 @@ class Public::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
 end
