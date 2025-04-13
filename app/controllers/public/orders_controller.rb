f95387be4a17e6352943eb043b5006d182187b5f
hayasemaru <mrym.124.hys@gmail.com>
2025-04-13 21:46:08 +0900

    edit order/new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,9 +3,37 @@ class Public::OrdersController < ApplicationController
 
   def new
     @customer = current_customer
+    @addresses = Address.all
   end
 
-  def confirm; end
+  def confirm
+    @order = Order.new(order_params)
+
+    case params[:order][:select_address]
+    when '0'
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    when '1'
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    when '2'
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    else
+      render :new and return
+    end
+
+    binding.pry
+    if @order.save
+      redirect_to order_path(order)
+    else
+      render :new
+    end
+  end
 
   def thanks; end
 
@@ -14,4 +42,10 @@ class Public::OrdersController < ApplicationController
   def index; end
 
   def show; end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
 end
