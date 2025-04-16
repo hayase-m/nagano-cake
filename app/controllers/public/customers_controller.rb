85cb9b2ea42f9e4cecec34f319c83cc7e1203379
hayasemaru <mrym.124.hys@gmail.com>
2025-04-17 00:10:50 +0900

    退会機能の実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,18 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
-  def show
-  end
 
-  def edit
-  end
+  def show; end
 
-  def update
-  end
+  def edit; end
 
-  def unsubscribe
-  end
+  def update; end
+
+  def unsubscribe; end
 
   def withdraw
+    current_customer.update(is_active: false)
+    reset_session
+    flash[:notice] = '退会処理が完了しました。'
+    redirect_to root_path
   end
 end
