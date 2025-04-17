6045ed9a1c99cedecfe78883dc09fb57caeb10ea
hayasemaru <mrym.124.hys@gmail.com>
2025-04-18 01:01:39 +0900

    配送先をポストできるように設定

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,31 @@
 class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
+    @address = Address.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash[:notice] = '配送先を設定しました'
+      redirect_to addresses_path
+    else
+      flash[:notice] = '配送先を設定できませんでした'
+      render :index
+    end
   end
 
-  def update
-  end
+  def update; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
