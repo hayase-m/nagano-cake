7fd873f915517bdbd8dbcdda11c9a4fbb1339a24
hayasemaru <mrym.124.hys@gmail.com>
2025-04-19 13:08:25 +0900

    配送先編集画面を追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,7 +6,9 @@ class Public::AddressesController < ApplicationController
     @addresses = Address.all
   end
 
-  def edit; end
+  def edit
+    @address = Address.find(params[:id])
+  end
 
   def create
     @address = Address.new(address_params)
@@ -15,12 +17,21 @@ class Public::AddressesController < ApplicationController
       flash[:notice] = '配送先を設定しました'
       redirect_to addresses_path
     else
-      flash[:notice] = '配送先を設定できませんでした'
+      flash[:notice] = '配送先を設定に失敗しました'
       render :index
     end
   end
 
-  def update; end
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = '配送先を更新しました'
+      redirect_to addresses_path
+    else
+      flash[:notice] = '配送先を更新に失敗しました'
+      render :edit
+    end
+  end
 
   def destroy
     @address = Address.find(params[:id])
