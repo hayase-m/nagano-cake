5b79080f2f0736ba0935f4acb6d0795b6f81ed12
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-05-15 15:34:23 +0900

    create genre/index

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,14 +1,27 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genres = Genre.all
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      render :index
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
+  def update; end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
--- db/migrate/20250226145634_devise_create_customers.rb
+++ db/migrate/20250226145634_devise_create_customers.rb
@@ -4,8 +4,8 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
@@ -14,14 +14,13 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Rememberable
       t.datetime :remember_created_at
 
-      t.string :last_name,null: false
-      t.string :first_name,null: false
-      t.string :last_name_kana,null: false
-      t.string :first_name_kana,null: false
-      t.string :is_active,null: false, default: true
-      t.string :postal_code,null: false
-      t.string :address,null: false
-      t.string :telephone_number,null: false
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :telephone_number, null: false
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
@@ -39,7 +38,6 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
