f180e3372b8f528bca5d6d152138597d7a9a3e29
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-05-15 16:18:23 +0900

    顧客詳細表示機能を追加し、顧客情報の編集および更新機能を実装しました。また、顧客詳細ページのデザインを更新しました。

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,9 +4,28 @@ class Admin::CustomersController < ApplicationController
     @customers = Customer.order(created_at: :asc).page(params[:page]).per(10)
   end
 
-  def show; end
+  def show
+    @customer = Customer.find(params[:id])
+  end
 
-  def edit; end
+  def edit
+    @customer = Customer.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      flash.now[:alert] = '更新に失敗しました'
+      render :edit
+    end
+  end
+end
+
+private
+
+def customer_params
+  params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address,
+                                   :telephone_number, :email, :is_active)
 end
